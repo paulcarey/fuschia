d1b35bb47602a18cde49b8ef194135defda8838f
Paul Carey <paul.p.carey@gmail.com>
2009-01-12 15:13:23 +0000

    Print error if design doc already exists

--- upload_binaries.sh
+++ upload_binaries.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DB_NAME=your_db_name
+set -u
+
+DB_NAME=$1
 
 DESIGN_DOC=localhost:5984/${DB_NAME}/_design%2Ffuschia
 
@@ -9,7 +11,13 @@ HTML='Content-Type:text/html'
 SWF='Content-Type:application/x-shockwave-flash'
 CSS='Content-Type:text/css'
 
-# Will intentionally fail if a design doc named fuschia already exists
+RES=`curl -s $DESIGN_DOC | grep not_found`
+if [ $? != 0 ]; then
+  echo "${DESIGN_DOC} exists!"
+  echo It must be deleted before running this script
+  exit 1
+fi
+
 REV=`curl -s -T bin/fuschia_queries.json $DESIGN_DOC | egrep -o '[0-9]+'`
 REV=`curl -s -H $JS -T bin/AC_OETags.js ${DESIGN_DOC}/AC_OETags.js?rev=${REV} | egrep -o '[0-9]+'`
 REV=`curl -s -H $HTML -T bin/Fuschia.html ${DESIGN_DOC}/Fuschia.html?rev=${REV} | egrep -o '[0-9]+'`
